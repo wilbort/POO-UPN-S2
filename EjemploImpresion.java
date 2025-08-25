1705da9b2d546bb17c4a97c25b61f932d3a6a631
Wilbort Encomenderos <wilbort@gmail.com>
2025-08-25 14:23:15 -0500

    7 - Ejemplo Impresión

---
+++ EjemploImpresion.java
@@ -0,0 +1,17 @@
+package ejemploimpresion;
+
+public class EjemploImpresion {
+
+    public static void main(String[] args) {
+        int numero = 25; // entero
+        double decimal = 3.14; // decimal
+        char letra = 'A'; // carácter
+        String texto = "Hola Mundo"; // cadena
+
+        System.out.println("Numero entero: " + numero);
+        System.out.println("Numero decimal: " + decimal);
+        System.out.println("Caracter: " + letra);
+        System.out.println("Texto: " + texto);
+
+    }    
+}
